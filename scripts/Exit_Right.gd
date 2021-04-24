1d7262aaa9597aebc704a1753fcd1bcc9162dc0d
Matt Penny <mwpenny@users.noreply.github.com>
2021-04-24 15:40:17 -0400

    Dynamic room loading

---
+++ scripts/Exit_Right.gd
@@ -0,0 +1,10 @@
+extends Area
+
+signal exited_left(exit_position)
+signal exited_right(exit_position)
+
+func _on_entered(body: Node):
+	emit_signal("exited_right", $Shape.translation)
+
+func _ready():
+	connect("body_entered", self, "_on_entered")
---
+++ scripts/RoomManager.gd
@@ -0,0 +1,82 @@
+extends Spatial
+
+const FLOOR_HEIGHT = 0.75
+
+enum EntranceDirection {
+	NONE,
+	LEFT,
+	RIGHT
+}
+
+onready var loaded_rooms = get_node("LoadedRooms")
+var active_room = null
+
+func get_active_navmesh():
+	if not active_room:
+		return null
+	return active_room.get_node("Navigation")
+
+func load_room(room_path: String, parent_exit_position: Vector3, entrance_direction = EntranceDirection.NONE):
+	var room_resource = load(room_path)
+	var room_scene = room_resource.instance()
+
+	var spawn_position = Vector3.ZERO
+
+	# Line up exit and entrance
+	if parent_exit_position and entrance_direction != EntranceDirection.NONE:
+		var entrance_node_name
+		if entrance_direction == EntranceDirection.LEFT:
+			spawn_position = right_of(active_room, parent_exit_position)
+			entrance_node_name = "Entrance_Left"
+		else:
+			spawn_position = left_of(active_room, parent_exit_position)
+			entrance_node_name = "Entrance_Right"
+
+		var entrance_position = room_scene.get_node(entrance_node_name).translation
+		spawn_position -= entrance_position
+
+	room_scene.translation = spawn_position
+	add_child_below_node(loaded_rooms, room_scene)
+	return room_scene
+
+func set_active_room(new_active_room: Spatial):
+	if active_room:
+		for exit in active_room.get_nodes("Exits").get_children():
+			exit.disconnect("exited_left", self, "_on_exit_left")
+			exit.disconnect("exited_right", self, "_on_exit_right")
+
+	for exit in new_active_room.get_node("Exits").get_children():
+		exit.connect("exited_left", self, "_on_exit_left")
+		exit.connect("exited_right", self, "_on_exit_right")
+
+	active_room = new_active_room
+
+func get_room_aabb(room: Spatial) -> AABB:
+	var mesh: MeshInstance = room.find_node("RoomMesh");
+	return mesh.get_aabb();
+
+func left_of(parent_room: Spatial, exit_position: Vector3) -> Vector3:
+	var aabb = get_room_aabb(parent_room)
+	return Vector3(exit_position.x, -FLOOR_HEIGHT, aabb.size.z)
+
+func right_of(parent_room: Spatial, exit_position: Vector3) -> Vector3:
+	var aabb = get_room_aabb(parent_room)
+	return Vector3(aabb.size.x, -FLOOR_HEIGHT, exit_position.z)
+
+func _on_exit_left(exit_position: Vector3):
+	load_room(
+		"res://rooms/Room1.tscn",
+		exit_position,
+		EntranceDirection.RIGHT
+	)
+
+func _on_exit_right(exit_position: Vector3):
+	load_room(
+		"res://rooms/Room1.tscn",
+		right_of(active_room, exit_position),
+		EntranceDirection.LEFT
+	)
+
+func _ready():
+	var room1 = load_room("res://rooms/Room1.tscn", Vector3.ZERO)
+	set_active_room(room1)
