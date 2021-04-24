f4c58a9d15cc2b9da1ba8ebc2ca6d2437ae73380
Matt Diener <mattndiener@gmail.com>
2021-04-24 16:13:17 -0400

    Basic character animation

--- Player.gd
+++ Player.gd
@@ -1,41 +1,113 @@
 extends KinematicBody
 
 var moveSpeed = 3.0
+var crouchMoveSpeed = 1.0
 var velocity = Vector3.ZERO
+var direction = Vector3.ZERO
+var moving = false
+var enteringCrouch = false
+var crouching = false
+var wasCrouching = false
+var crouchFrames = 0
+var crouchTime = 0.416
 
 onready var room_manager = get_node("../RoomManager")
+onready var character_animation = get_node("Character/Animation")
+onready var character = get_node("Character")
+
+enum PlayerAnimations { 
+	CROUCH, 
+	CROUCH_IDLE, 
+	CROUCH_WALK, 
+	DEATH, 
+	IDLE, 
+	INTERRACT, 
+	RUN, 
+	SHOOT, 
+	RUN_SHOOT,
+	CROUCH_SHOOT
+	CROUCH_WALK_SHOOT
+}
 
 func get_input():
-	var input = Vector3.ZERO
+	var tmp_direction = Vector3.ZERO
 	
 	if Input.is_action_pressed("move_forward"):
-		input.x -= 1
-		input.z -= 1
+		tmp_direction.x -= 1
+		tmp_direction.z -= 1
 	if Input.is_action_pressed("move_backward"):
-		input.x += 1
-		input.z += 1
+		tmp_direction.x += 1
+		tmp_direction.z += 1
 	if Input.is_action_pressed("move_left"):
-		input.x -= 1
-		input.z += 1
+		tmp_direction.x -= 1
+		tmp_direction.z += 1
 	if Input.is_action_pressed("move_right"):
-		input.x += 1
-		input.z -= 1
+		tmp_direction.x += 1
+		tmp_direction.z -= 1
 	
-	return input.normalized()
-	
+	moving = false;
+	if tmp_direction.x != 0 or tmp_direction.z != 0:
+		moving = true
+		direction = tmp_direction
+		
+	crouching = false
+	if Input.is_action_pressed("crouch"):
+		crouching = true
 
-func _physics_process(delta):	
-	var input = get_input()
-	var direction = ((transform.basis.z * input.z) + (transform.basis.x * input.x))
+func handle_crouch(delta):
+	if not crouching:
+		wasCrouching = false
+		return
+		
+	if wasCrouching:
+		crouchFrames += delta
+	else:
+		crouchFrames = 0
+		wasCrouching = true
 	
-	velocity.x = direction.x * moveSpeed
-	velocity.z = direction.z * moveSpeed
+	if crouchFrames >= crouchTime:
+		enteringCrouch = false
+	else:
+		enteringCrouch = true
+		
+func derive_animation_state():
+	if crouching and enteringCrouch:
+		character_animation.set("parameters/Transition/current", PlayerAnimations.CROUCH)
+	elif crouching and not moving:
+		character_animation.set("parameters/Transition/current", PlayerAnimations.CROUCH_IDLE)
+	elif crouching:
+		character_animation.set("parameters/Transition/current", PlayerAnimations.CROUCH_WALK)
+	elif moving:
+		character_animation.set("parameters/Transition/current", PlayerAnimations.RUN)
+	else:
+		character_animation.set("parameters/Transition/current", PlayerAnimations.IDLE)
+
+func _physics_process(delta):
+	get_input()
+	
+	var dir = ((transform.basis.z * direction.z) + (transform.basis.x * direction.x))
+	
+	handle_crouch(delta)
+	
+	var currentMoveSpeed = 0
+	if moving:
+		character.look_at(translation - dir, Vector3(0,1,0)) 
+		
+		currentMoveSpeed = moveSpeed
+		if crouching:
+			currentMoveSpeed = crouchMoveSpeed
+	
+	velocity.x = dir.x * currentMoveSpeed
+	velocity.z = dir.z * currentMoveSpeed
 	
 	move_and_collide(velocity * delta)
 
 	var nav_mesh = room_manager.get_active_navmesh()
 	if nav_mesh:
 		translation = nav_mesh.get_closest_point(translation)
+		
+	derive_animation_state()
+	print(character_animation.get("parameters/Transition/current"))
 
 
 # Declare member variables here. Examples:
@@ -45,6 +117,8 @@ func _physics_process(delta):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	character_animation.active = true
+	character_animation.set("parameters/Transition/current", "Idle")
 	pass # Replace with function body.
 
 
