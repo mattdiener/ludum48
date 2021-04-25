1d8e74813526ea8de57d52ceb080ad4bcdc79caa
Matt Penny <mwpenny@users.noreply.github.com>
2021-04-25 12:47:30 -0400

    HACK: Make NPCs touch floor by moving them down

--- scripts/Weapon.gd
+++ scripts/Weapon.gd
@@ -3,7 +3,9 @@ extends Spatial
 var shooting = false
 
 func begin_shoot():
-	shooting = true
+	# For now, shoot all the time
+	if not shooting:
+		shooting = true
 
 func end_shoot():
 	shooting = false
